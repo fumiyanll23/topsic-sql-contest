9e7c569d92d30190344a024d6b492daa574e124e
fumiyanll23 <fumiyanll23.public@gmail.com>
2023-04-15 16:43:32 +0900

    Initial commit

---
+++ practice/02_ans.sql
@@ -0,0 +1,16 @@
+SELECT
+    HP.PF_CODE AS 都道府県コード
+    , PF.PF_NAME AS 都道府県名
+    , ROUND(
+        CAST(HP.INP_YES AS REAL) /
+        CAST( (HP.INP_YES + HP.INP_NO + HP.UNIDENTIFIED) AS REAL )
+        * 100
+        , 1
+    ) AS 入院率
+FROM
+    HOSPITALIZATION AS HP
+    INNER JOIN PREFECTURE AS PF
+        ON PF.PF_CODE = HP.PF_CODE
+ORDER BY
+    入院率 DESC
+    , HP.PF_CODE ASC;
